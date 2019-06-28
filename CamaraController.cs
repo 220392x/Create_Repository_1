e3d4c7ae7c01732a7cddc83cb3bc805175850171
220392x <yamato0516hirokazu@icloud.com>
2019-06-28 10:58:38 +0900

    とりあえずテスト用

---
+++ CamaraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject player;
+
+	void Start () {
+        this.player = GameObject.Find("Furry");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(playerPos.x, transform.position.y, transform.position.z);
+	}
+}
